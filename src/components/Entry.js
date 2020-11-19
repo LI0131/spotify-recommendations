974ee4cd32e924d37fe91bfc5cffcbd8163afd6f
LI0131 <mccannliam.lm@gmail.com>
2020-11-18 21:45:32 -0500

    Clean up and remove localhost references

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,17 +1,17 @@
 import { Bullseye, Button } from '@patternfly/react-core';
 import { useWindowDimensions } from '../utils/windowHook';
-import { useHistory } from 'react-router-dom';
 
 const urlBase = 'https://accounts.spotify.com/authorize?response_type=token';
 const clientId = 'client_id=71e1905d849e4a5ba826d60bc1b78015';
 const scope = 'scope=user-top-read'
-const redirectUri = 'redirect_uri=http://localhost:3000/redirect'
+const redirectUri = (origin) => `redirect_uri=${origin}/redirect`
 
 const Entry = () => {
     const windowDimensions = useWindowDimensions();
+    const { origin } = window.location;
 
     return <Bullseye style={windowDimensions}>
-        <Button onClick={() => (window.location = `${urlBase}&${clientId}&${scope}&${redirectUri}`)}>
+        <Button onClick={() => (window.location = `${urlBase}&${clientId}&${scope}&${redirectUri(origin)}`)}>
             Get Music Recommendations
         </Button>
     </Bullseye>;
